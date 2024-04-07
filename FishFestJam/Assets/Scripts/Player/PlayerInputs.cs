6843b5f901b77e6480a21a355638be1a96d09333
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-04-06 17:49:14 -0700

    update movement

--- FishFestJam/Assets/Scripts/Player/PlayerInputs.cs
+++ FishFestJam/Assets/Scripts/Player/PlayerInputs.cs
@@ -22,9 +22,17 @@ public class PlayerInputs : MonoBehaviour
         Move();
     }
 
-    private void Move() {
-        float moveVelocityX = playerControls.Movement.Horizontal.ReadValue<float>() * moveSpeed;
-        float moveVelocityY = playerControls.Movement.Vertical.ReadValue<float>() * moveSpeed;
-        rb.AddForce(new Vector2(moveVelocityX, moveVelocityY),ForceMode2D.Force);
+    // Moves with respect of up pointing towards the top of the screen
+    // private void Move() {
+    //     float moveVelocityX = playerControls.Movement.Horizontal.ReadValue<float>() * moveSpeed;
+    //     float moveVelocityY = playerControls.Movement.Vertical.ReadValue<float>() * moveSpeed;
+    //     rb.AddForce(new Vector2(moveVelocityX, moveVelocityY),ForceMode2D.Force);
+    // }
+
+    // Moves with respect of up is where the player is looking
+    private void Move(){
+        Vector2 horizontalVelocity = moveSpeed * playerControls.Movement.Horizontal.ReadValue<float>() * transform.right;
+        Vector2 verticalVelocity = moveSpeed * playerControls.Movement.Vertical.ReadValue<float>() * transform.up;
+        rb.AddForce(horizontalVelocity + verticalVelocity,ForceMode2D.Force);
     }
 }
