54053a07fa4b512c98cd029ba169efbc926c9f19
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-04-22 20:43:26 -0700

    dummy displays dps text

--- FishFestJam/Assets/Scripts/Enemy/TrainingDummy.cs
+++ FishFestJam/Assets/Scripts/Enemy/TrainingDummy.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrainingDummy : EnemyBehavior
 {
     private Timer DPStimer;
+    private TextMeshPro text;
     float damageCount = 0f;
     protected override void Awake()
     {
+        text = GetComponentInChildren<TextMeshPro>();
         DPStimer = GetComponent<Timer>();
-        DPStimer.SetTimer(1, () => {DisplayDPS();});
+        DPStimer.SetTimer(1, () => {ResetDamageCount();});
     }
     protected override void Update()
     {
-        DPStimer.SetTimer(1, () => {DisplayDPS();});
+        DPStimer.SetTimer(1, () => {ResetDamageCount();});
     }
     protected override void OnParticleCollision(GameObject other) {
         damageCount += other.GetComponent<BaseWeapon>().BaseDamage * other.GetComponent<BaseWeapon>().WeaponMultiplier;
     }
-    private void DisplayDPS(){
-        Debug.Log($"DPS: {damageCount}");
+    private void ResetDamageCount(){
+        text.text = $"DPS: {damageCount}";
         damageCount = 0;
     }
 }
