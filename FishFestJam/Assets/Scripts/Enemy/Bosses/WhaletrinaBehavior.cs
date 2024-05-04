a0d998d964fa83526e0078d2d20ec0408ccba0a3
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-05-04 13:43:26 -0700

    start on whaletrina

---
+++ FishFestJam/Assets/Scripts/Enemy/Bosses/WhaletrinaBehavior.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class WhaletrinaBehavior : EnemyBehavior
+{
+    [TitleGroup("Attack Commands")]
+    
+    public void FishFoodSpray(){
+        
+    }
+}
